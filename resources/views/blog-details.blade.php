d9312571deb3b1a2af70207242c903074a7cfe8c
HPJM3018 <judithhouinsou0@gmail.com>
2023-08-14 14:50:52 +0200

    mise en forme

--- resources/views/blog-details.blade.php
+++ resources/views/blog-details.blade.php
@@ -3,19 +3,17 @@
 @section('content')
 
 <section class="page-header">
-            <div class="page-header-bg" style="background-image: url(assets/images/backgrounds/page-header-bg.jpg)">
+        <div class="container">
+            <div class="page-header__inner">
+                <ul class="thm-breadcrumb list-unstyled">
+                    <li><a href="{{ route('home') }}">Home</a></li>
+                    <li><span>/</span></li>
+                    <li class="active">News</li>
+                </ul>
+                <h2>Latest news</h2>
             </div>
-            <div class="container">
-                <div class="page-header__inner">
-                    <ul class="thm-breadcrumb list-unstyled">
-                        <li><a href="index.html">Home</a></li>
-                        <li><span>/</span></li>
-                        <li class="active">Donations details</li>
-                    </ul>
-                    <h2>Donations details</h2>
-                </div>
-            </div>
-        </section>
+        </div>
+</section>
         <!--Page Header End-->
 
         <!--Donation Details Start-->
